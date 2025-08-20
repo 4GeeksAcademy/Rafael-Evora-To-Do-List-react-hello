f913a2ab957c06b7c2cecacf528391ded36e06e3
rafaelevora <rafael_evora@yahoo.com.mx>
2025-08-20 17:43:41 +0000

    raf

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,26 +1,43 @@
-import React from "react";
+import React, {useState } from "react";
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
+
 const Home = () => {
+	const [input, setInput] = useState("");
+	const [tasks, setTasks] = useState([]);
+
+	const addTask = () => {
+		if (input.trim() !== "") {
+			setTasks([...tasks, input]);
+			setInput("");
+		}
+	};
+
+
+
+
 	return (
 		<div className="text-center">
-            
-
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<h1>todos</h1>
+			<div>
+				<input
+					type="text"
+					onChange={e => setInput(e.target.value)}
+					value={input}
+					placeholder="Add a new task"
+					onKeyDown={e => { if (e.key === 'Enter') addTask(); }}
+				/>
+				<button onClick={addTask}>Add</button>
+				<ul id="task-list" className="card">
+					{tasks.map((task, idx) => (
+						<li key={idx}>{task}</li>
+					))}
+				</ul>
+			</div>
 		</div>
 	);
 };
